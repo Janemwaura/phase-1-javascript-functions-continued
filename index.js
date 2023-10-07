af6876739576bb7d5a28b2fd2f81b95c3c1871df
Janemwaura <mwaura461@gmail.com>
2023-10-07 23:05:37 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller skating"){
+    console.log(`This Saturday I want to go for ${activity}.`);
+     console.log(`This Saturday I want to go for ${activity}.`);
+}
+
+saturdayFun();
+console.log(saturdayFun("bathing my dog"));
+(function (){ console.log("Yet more razzling")
+
+}
+);
+
+const fn = function mondayWork (agenda = "go to the office") {
+    console.log(`This Monday I will ${agenda}.`);
+}
+fn();
+
+function outer(message = "You are"){
+return function(feeling = "*"){
+return`${message},${feeling}`;
+};
+}
+console.log(outer()("*"));
